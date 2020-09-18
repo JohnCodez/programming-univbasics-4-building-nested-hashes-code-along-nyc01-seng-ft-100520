b5f067756938e5753fb1639a5e2485c04d01f7fd
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-09-18 18:12:21 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -3,11 +3,11 @@ def second_challenge
     :montague => {
       {:patriarch => {} , :matriarch => {} , :hero => {} , :hero_friend => []
       }, 
-    :capulet => {:patriarch => {} , :matriarch => {} , :heroine => {} , :heroine_friend => []
+    :capulet => {:patriarch => {} , :matriarch => {} , :heroine => {} , :heroine_friend => []}
       }
     }
-    }
-    }
+    
+    
 
   
 end
