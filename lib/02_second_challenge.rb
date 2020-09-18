66de8c137496e3671bd28ec8cfead15acb68437b
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-09-18 18:15:53 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,7 +1,6 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarch => {} , :matriarch => {} , :hero => {} , :hero_friend => []}
-      , 
+    :montague => {:patriarch => {} , :matriarch => {} , :hero => {} , :hero_friend => []}, 
     :capulet => {:patriarch => {} , :matriarch => {} , :heroine => {} , :heroine_friend => []}
       }
 end
